9b6759e6172c305f77f187e686452cf8eab5229a
sgm0256 <123716216+sgm0256@users.noreply.github.com>
2024-07-18 00:44:13 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int date, month, year;
+    char a, b;
+    cin >> year >> a >> date >> b >> month;
+    cout << date << "-" << month << "-" << year;
+    return 0;
+}
