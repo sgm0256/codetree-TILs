0eabb3eca4f4354eda614c9453d576f29054b5b5
sgm0256 <123716216+sgm0256@users.noreply.github.com>
2024-07-18 00:57:18 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(2);
+    cout << (a+b)/(a-b);
+    return 0;
+}
