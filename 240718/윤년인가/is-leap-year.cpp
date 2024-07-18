7a396927794533a20f20280fa4cb68fa56ba0be8
sgm0256 <123716216+sgm0256@users.noreply.github.com>
2024-07-18 11:48:10 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/윤년인가/is-leap-year.cpp
+++ 240718/윤년인가/is-leap-year.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int y;
     cin >> y;
-    cout << (((y%4==0)||((y%100)==0)&&(y%400!=0)) ? "true" : "no");
+    cout << ((y%4==0) ? (((y%100==0)&&(y%400!=0)) ? "false" : "true") : "false");
     return 0;
 }
