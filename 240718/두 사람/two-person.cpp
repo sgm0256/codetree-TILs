8b7c3ae5509dc9435455aab3cbd31191c064d5b2
sgm0256 <123716216+sgm0256@users.noreply.github.com>
2024-07-18 11:32:42 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/두 사람/two-person.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age1, age2;
+    char sex1, sex2;
+    cin >> age1 >> sex1 >> age2 >> sex2;
+    cout << (((age1>=19)&&(sex1=='M'))||((age2>=19)&&(sex2=='M')));
+    return 0;
+}
