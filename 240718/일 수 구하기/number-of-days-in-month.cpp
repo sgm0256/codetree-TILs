213f7394e74c8fea47d70d2f880d45d8108d0dd4
sgm0256 <123716216+sgm0256@users.noreply.github.com>
2024-07-18 11:55:11 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/일 수 구하기/number-of-days-in-month.cpp
+++ 240718/일 수 구하기/number-of-days-in-month.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    cout << ((n%2==1)?31:((n==2)?28:30));
+    cout << ((n<=7)?((n%2==1)?31:((n==2)?28:30)):((n%2==0)?31:30));
     return 0;
 }
